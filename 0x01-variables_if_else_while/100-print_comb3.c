430c8624b4bcebcf68074379e3c13ae853f8448b
Your Name <you@example.com>
2022-07-01 14:46:44 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,15 @@ for (c = '0'; c <= '9'; c++)
 {
 for (i = '0'; i <= '9'; i++)
 {
+if (c < i)
+{
 putchar(c);
 putchar(i);
 if ((c != '9') || (c == '9' && i != '9'))
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
